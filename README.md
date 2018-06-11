d63dbb68be87c4c9a7dbb800dfcc24dbc9edd7b7
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2018-06-11 19:47:31 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+https://unsplash.com/photos/B8ZFV4E0YOw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require( 'gulp' );
+const PathMap = require( 'sfco-path-map' );
+const pug = require( 'gulp-pug' );
+const sass = require( 'gulp-sass' );
+
+const PATHS = new PathMap( {
+	src: './src',
+	dest: './dist',
+	imagesSrc: '{{src}}/img',
+	imagesDest: '{{dest}}/img',
+	stylesSrc: '{{src}}/styles',
+	stylesDest: '{{dest}}/css',
+	viewsSrc: '{{src}}/views',
+	viewsDest: '{{dest}}',
+} );
+
+gulp.task( 'default', [ 'styles', 'views' ] );
+
+gulp.task( 'images', () => {
+	return gulp.src( `${PATHS.imagesSrc}/**/*` )
+		.pipe( gulp.dest( PATHS.imagesDest ) );
+} );
+
+gulp.task( 'styles', () => {
+	return gulp.src( `${PATHS.stylesSrc}/styles.scss` )
+		.pipe( sass( {
+			outputStyle: 'expanded',
+		} ) )
+		.pipe( gulp.dest( PATHS.stylesDest ) );
+} );
+
+gulp.task( 'views', () => {
+	return gulp.src( `${PATHS.viewsSrc}/*.pug` )
+		.pipe( pug() )
+		.pipe( gulp.dest( `${PATHS.viewsDest}/` ) ); // NOTE: Omitting the trailing slash results in `pug` creating a duplicate `dist/` dir.
+} );
+
+gulp.task( 'watch', [ 'default' ], () => {
+	gulp.watch( `${PATHS.stylesSrc}/**/*.scss`, [ 'styles' ] );
+	gulp.watch( `${PATHS.viewsSrc}/**/*.pug`, [ 'views' ] );
+} );
